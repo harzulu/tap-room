255636f4526e320fb4c705942c9a121c8a96e4d2
Benjamin McFarland <benrmcfarland@gmail.com>
2021-02-12 11:07:14 -0800

    Add basic controller, and feature to create new keg

--- src/components/BarController.js
+++ src/components/BarController.js
@@ -1,18 +1,45 @@
 import React from 'react';
+import KegList from './KegList';
+import NewKegForm from './NewKegForm.js';
 
 export default class BarController extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      masterKegList = [],
-      currentKeg = null
+      masterKegList: [],
+      currentKeg: null,
+      currentViewPage: true
     }
   }
 
+  handleNewKegCreation = (newKeg) => {
+    const newMasterKegList = this.state.masterKegList.concat(newKeg);
+    this.setState({
+      masterKegList: newMasterKegList,
+      currentViewPage: false
+    })
+  }
+
+  handleClick = () => {
+    this.setState({currentViewPage: !this.state.currentViewPage});
+  }
+
   render() {
+    let currentVisibleState = null;
+    let buttonText = null;
+
+    if (this.state.currentViewPage) {
+      currentVisibleState = <NewKegForm onNewKegCreation={this.handleNewKegCreation}/>
+      buttonText = "Return to keg list";
+    } else {
+      currentVisibleState = <KegList kegList={this.state.masterKegList} />
+      buttonText = "Add new keg";
+    } 
+
     return (
       <>
-        
+        {currentVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </>
     );
   }
