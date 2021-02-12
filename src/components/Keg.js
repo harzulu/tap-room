104e1f782f0b96d36baa509e0010bd5c05368a98
Benjamin McFarland <benrmcfarland@gmail.com>
2021-02-12 15:34:51 -0800

    Add colors to list page

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -2,12 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Keg(props) {
+  let colorId, pintsId;
+  let kegCost = parseInt(props.price);
+
+  if (kegCost > 0 && kegCost <= 15) {
+    colorId = "green";
+  } else if (kegCost > 15 && kegCost <= 25) {
+    colorId = "yellow";
+  } else if (kegCost > 25 && kegCost <= 35) {
+    colorId = "orange";
+  } else {
+    colorId = "red";
+  }
+
+  if (props.pints <= 10 && props.pints > 0) {
+    pintsId = "orange";
+  } else if (props.pints <= 0) {
+    pintsId = "red";
+  } else {
+    pintsId = "green";
+  }
+
   return (
     <>
     <div onClick={() => props.whenKegClicked(props.id)}>
       <h3>{props.name}</h3>
-      <h3>${props.price}</h3>
-      <h3>Pints left: {props.pintsg}</h3>
+      <div class="price" id={colorId}>
+        <h3>${props.price}</h3>
+      </div>
+      <div class="price" id={pintsId}>
+        <h3>Pints left: {props.pints}</h3>
+      </div>
     </div>
     </>
   );
