8cc0bfddac4b2a1aa1794843f91ddd856c8b612d
Benjamin McFarland <benrmcfarland@gmail.com>
2021-02-12 17:09:21 -0800

    Updates

---
+++ precache-manifest.b7283af6a32f163668513a16fdcbbe7d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b07f4837bc57bf9371ae564324b1a9be",
+    "url": "/tap-room/index.html"
+  },
+  {
+    "revision": "31c970b8970d0fdc493c",
+    "url": "/tap-room/static/css/2.87a9c7a2.chunk.css"
+  },
+  {
+    "revision": "b3a1bf6d0eeba7d599dc",
+    "url": "/tap-room/static/css/main.aa03449b.chunk.css"
+  },
+  {
+    "revision": "31c970b8970d0fdc493c",
+    "url": "/tap-room/static/js/2.8263a701.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/tap-room/static/js/2.8263a701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97b30126cae65c3c8b98",
+    "url": "/tap-room/static/js/3.94295b54.chunk.js"
+  },
+  {
+    "revision": "b3a1bf6d0eeba7d599dc",
+    "url": "/tap-room/static/js/main.97ab975e.chunk.js"
+  },
+  {
+    "revision": "d1f71e913293fb58f721",
+    "url": "/tap-room/static/js/runtime-main.c9fcce2a.js"
+  }
+]);
